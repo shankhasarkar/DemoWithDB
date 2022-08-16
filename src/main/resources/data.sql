f27378ed76e3e228fa9efc3362a373d934f9686f
Ritam Sarkar <ritamsarkar@Ritams-MBP.lan>
2022-08-16 03:10:23 -0500

    push

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO wfa_customer (loyalty_id, first_name, last_name, birth_date) VALUES ('WFA-100', 'Scott', 'Parkar', '1980-06-01');
-INSERT INTO wfa_customer (loyalty_id, first_name, last_name, birth_date) VALUES ('WFA-101', 'John', 'Page', '1980-09-01');
-INSERT INTO wfa_customer (loyalty_id, first_name, last_name, birth_date) VALUES ('WFA-102', 'Randy', 'Hall', '1980-07-01');
-INSERT INTO wfa_customer (loyalty_id, first_name, last_name, birth_date) VALUES ('WFA-103', 'Vincent', 'Gonzo', '1980-08-01');
+--INSERT INTO wfa_customer (loyalty_id, first_name, last_name, birth_date) VALUES ('WFA-100', 'Scott', 'Parkar', '1980-06-01');
+--INSERT INTO wfa_customer (loyalty_id, first_name, last_name, birth_date) VALUES ('WFA-101', 'John', 'Page', '1980-09-01');
+--INSERT INTO wfa_customer (loyalty_id, first_name, last_name, birth_date) VALUES ('WFA-102', 'Randy', 'Hall', '1980-07-01');
+--INSERT INTO wfa_customer (loyalty_id, first_name, last_name, birth_date) VALUES ('WFA-103', 'Vincent', 'Gonzo', '1980-08-01');
