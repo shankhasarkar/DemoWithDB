dea9770c325ea11a799d7522f0b48f52d2271065
Ritam Sarkar <ritamsarkar@Ritams-MBP.lan>
2022-08-16 02:35:08 -0500

    push

--- src/main/java/com/dv/demo/ApplicationService.java
+++ src/main/java/com/dv/demo/ApplicationService.java
@@ -1,8 +1,5 @@
 package com.dv.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,14 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class ApplicationService {
-    static Logger log = LoggerFactory.getLogger(ApplicationService.class.getName());
-
-    @Value("${spring.jpa.hibernate.ddl-auto}")
-    private static String ddlAuto;
-
     public static void main(String[] args) {
         SpringApplication.run(ApplicationService.class, args);
-        System.out.println("Rajarsi" + ddlAuto);
-        log.info("Rajarsi" + ddlAuto);
     }
 }
