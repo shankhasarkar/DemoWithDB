01ca65927b2a3def01c09dc4435946d7ae59551a
Ritam Sarkar <ritamsarkar@Ritams-MBP.lan>
2022-08-17 11:25:31 -0500

    nth push

--- src/main/java/com/dv/demo/ApplicationService.java
+++ src/main/java/com/dv/demo/ApplicationService.java
@@ -1,13 +1,28 @@
 package com.dv.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.util.concurrent.CountDownLatch;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class ApplicationService {
-    public static void main(String[] args) {
-        SpringApplication.run(ApplicationService.class, args);
+    @Autowired static CountDownLatch closeLatch;
+
+    public static void main(String[] args) throws InterruptedException {
+        ApplicationContext ctx = SpringApplication.run(ApplicationService.class, args);
+
+        closeLatch = ctx.getBean(CountDownLatch.class);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                closeLatch.countDown();
+            }
+        });
+        closeLatch.await();
     }
 }
--- src/main/java/com/dv/demo/customer/infrastructure/configs/AppConfig.java
+++ src/main/java/com/dv/demo/customer/infrastructure/configs/AppConfig.java
@@ -1,8 +1,14 @@
 package com.dv.demo.customer.infrastructure.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.CountDownLatch;
 
 @Configuration
 public class AppConfig {
-
+    @Bean
+    public CountDownLatch closeLatch() {
+        return new CountDownLatch(1);
+    }
 }
